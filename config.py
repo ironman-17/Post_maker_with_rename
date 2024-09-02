0500f512200831225e1d7a03a8a9785f88dbac4c
Rajps44 <singhayush88805@gmail.com>
2024-09-02 19:24:50 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+API_ID = 'your_api_id'
+API_HASH = 'your_api_hash'
+BOT_TOKEN = 'your_bot_token'
+MONGO_URI = 'your_mongodb_uri'
+LOG_CHANNEL_ID = -1001234567890
+POST_CHANNELS = [-1001234567891, -1001234567892]
+TIMEZONE = 'Asia/Kolkata'
