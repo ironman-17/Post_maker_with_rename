baf209d3622f758e7a5fb64e9f0752b6b242d8a8
Rajps44 <singhayush88805@gmail.com>
2024-09-02 20:09:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 
-API_ID = '25208218
+API_ID = '25208218'
 API_HASH = 'cc8e841203c92f077015b865f44a8663 api hash'
 BOT_TOKEN = '7478730845:AAF19Hkk5uQbb6NRD1_RKtwwEAKuFZ7NxEc'
 MONGO_URI = 'mongodb+srv://mohdkaif2082:kaif1344@cluster0.pr8z3dx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
