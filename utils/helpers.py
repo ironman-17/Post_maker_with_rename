7a1d3de5c3fb00ed5f417ec8509e355609c8f827
Rajps44 <singhayush88805@gmail.com>
2024-09-02 19:27:21 +0530

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -18,4 +18,17 @@ def get_greeting():
 
 async def post_to_channels(client: Client, text: str):
     for channel in POST_CHANNELS:
+async def post_to_channels(client: Client, text: str):
+    for channel in POST_CHANNELS:
+        await client.send_message(channel, text)
+
+async def log_to_channel(client: Client, message: str):
+    await client.send_message(LOG_CHANNEL_ID, message)
+
+async def handle_photo(client: Client, message: Message):
+    if message.photo:
+        # Implement photo auto-captioning logic here
+        caption = "Auto-captioned text here"
+        await client.send_message(message.chat.id, caption)
+        
       
