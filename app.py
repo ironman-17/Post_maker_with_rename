588b7ab068fc1a3bdff189938f121a4c5f44dd67
Rajps44 <singhayush88805@gmail.com>
2024-09-02 19:44:31 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+#@CoderRzps
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from CoderRzps'
+
+
+if __name__ == "__main__":
+    app.run()
