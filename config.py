973c0c128ff1918d6f5fab251bcac3e27e6370ab
Rajps44 <singhayush88805@gmail.com>
2024-09-02 22:11:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # config.py
 
-API_ID = 
+API_ID = ''
 API_HASH = ''  # Corrected line
 BOT_TOKEN = ''
 MONGO_URI = ''
-LOG_CHANNEL_ID = 
+LOG_CHANNEL_ID = ''
 POST_CHANNELS = []
 TIMEZONE = 'Asia/Kolkata'
