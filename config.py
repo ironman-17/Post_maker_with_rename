eccfd8965c868efb0e948c7c34c9115a710d2564
Raj <singhayush88805@gmail.com>
2024-10-02 11:46:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,10 @@ API_HASH = 'cc8e841203c92f077015b865f44a8663'  # Corrected line
 BOT_TOKEN = '7478730845:AAF19Hkk5uQbb6NRD1_RKtwwEAKuFZ7NxEc'
 OWNER_ID = 6174868004
 MONGO_URI = 'mongodb+srv://mohdkaif2082:kaif1344@cluster0.pr8z3dx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-LOG_CHANNEL_ID = [-1002192763590]
-POST_CHANNELS = [-1002053258285,-1002233212878,-1002193015686,-1002227906864]
+
+# Channels and IDs
+LOG_CHANNEL_ID = -1002192763590  # Single log channel
+POST_CHANNELS = [-1002053258285, -1002233212878, -1002193015686, -1002227906864]  # Multiple post channels
+
+# Timezone for scheduling and periodic tasks
 TIMEZONE = 'Asia/Kolkata'
